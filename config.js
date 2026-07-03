ce0eab396a0cf3db7ae5a959eb98ac2669ad6d0e
ozinet <55225048+ozinet@users.noreply.github.com>
2026-07-03 23:34:01 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
 // Ganti nilai di bawah ini dengan URL Web App Google Apps Script kamu
 // (didapat setelah Deploy > New deployment > Web app, lihat README.md)
 // =====================================================================
-const API_URL = "https://script.google.com/macros/s/AKfycbzxtPxV9RxzeK72eeoO0si9LMZbs-h2SjK5ZoFAXaKAUjY3ogUcrylUYT57IIwm9AqF/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycby35s1KSyV9Mo9VH-LW9UW9zcFmz5KT4F7l-hdwkQrw0VIpHgC20np9vijZzg5hamqM/exec";
